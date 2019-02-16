16218b53c996c928489b4bbd714e32144df4ed2d
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-16 21:12:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -53,7 +53,7 @@ end
 
 def shortened_tweet_truncator(tweet)
     if tweet.length > 140
-      word_substituter(tweet)[0..139] + "..."
+      word_substituter(tweet)[0..139] 
     else
       word_substituter(tweet)
     end
